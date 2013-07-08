516602da356630f5a17dcfbce351d44db321bea9
Teddy Knox <teddyknox@gmail.com>
2013-07-08 17:35:23 -0400

    fixing bugs

--- project_name/settings/common.py
+++ project_name/settings/common.py
@@ -205,7 +205,7 @@ THIRD_PARTY_APPS = (
 )
 
 LOCAL_APPS = (
-    'users',
+    'apps.users',
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
--- project_name/urls.py
+++ project_name/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import TemplateView
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
 admin.autodiscover()
@@ -10,7 +10,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Admin panel and documentation:
     url(r'^$', TemplateView.as_view(template_name='index.html')),
-    url(r'^', include('apps.static_pages.urls')),
     url(r"^account/", include("account.urls")),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
